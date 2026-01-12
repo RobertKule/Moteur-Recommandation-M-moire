320679643e1eba090d268010b918faa03a6f7c24
RobertKule <kulewakangitsirobert@gmail.com>
2026-01-12 12:07:58 +0200

    chore: Suppression des fichiers de build Next.js du suivi Git

--- NEXT_frontend/.next/dev/static/chunks/src_app_dashboard_layout_tsx_b1aff14b._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_f3b28941._.js",
-  "static/chunks/src_0bb4b367._.js"
-],
-    source: "dynamic"
-});
--- NEXT_frontend/.next/dev/static/chunks/src_app_dashboard_page_tsx_0208c902._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_16d3201b._.js"
-],
-    source: "dynamic"
-});
